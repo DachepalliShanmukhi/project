ec7fdfc0e1d612d43257b26a8d114a5cc59fc634
DachepalliShanmukhi <109751946+DachepalliShanmukhi@users.noreply.github.com>
2025-03-30 19:40:56 +0530

    Add files via upload

--- assignment3/ Employee Management System/Employee.java
+++ assignment3/ Employee Management System/Employee.java
@@ -1,1 +1,16 @@
 
+public class Employee {
+	int id;
+	String name;
+	double salary;
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+    public void displayDetails() {
+    	 System.out.printf("| %-5d | %-15s | %-10.2f |\n", id, name, salary);
+
+	}
+}
